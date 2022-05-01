c2b008f28591d3f55996695075de9882fac4b59b
Stefanie T <72820144+steffleupagus@users.noreply.github.com>
2022-05-01 02:45:09 +0000

    Adding Level Parse menu command

---
+++ handlers/commands/menuParseLevel.js
@@ -0,0 +1,50 @@
+const { ContextMenuCommandBuilder } = require('@discordjs/builders')
+const { ApplicationCommandType } = require(`../../utilities/enums.js`)
+const Utils = require(`../../utilities/utilFuncs.js`)
+const LevelData = require(`../../utilities/levelUtils.js`)
+
+const mod = process.env.mod || ""
+const config = require(`../../config/${mod}_config.json`)
+
+function shouldHandle(client, message)
+{
+	return LevelData.isLevelMessage(client, message)
+}
+
+function handle(client, message, interaction=null, sendResult=true)
+{
+	LevelData.logLevelMessage(client, message, interaction, sendResult)
+}
+
+async function execute(interaction)
+{
+	const client = interaction.client;
+	const messageId = interaction.targetId;
+	const channel = interaction.channel;
+	const message = await channel?.messages.fetch(messageId);
+	
+	// const roles = [ config.ModeratorRole, config.DMRole ];
+	// const hasRole = Utils.hasAnyRole(interaction.member, roles);
+
+	if (!message.author.bot)
+		interaction.guild.members.fetch(message.author)
+	
+	if (message && shouldHandle(client, message))
+	{
+		await interaction.reply({ 	content: 'Parsing for level/exp message', 
+									ephemeral: true });		
+		handle(client, message, interaction);
+	}else{	
+		await interaction.reply({ 	content: 'This is not a level or exp message.', 
+									ephemeral: true });
+	}
+}
+
+module.exports = 
+{
+	data: new ContextMenuCommandBuilder()
+		.setName('Parse Level')
+		.setType(ApplicationCommandType.Message),
+	execute: execute,
+	build:config.PRODUCTION || config.DEV
+};
