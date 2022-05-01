27c044dd9b07979a0ce89a1b81f7fdc9ddc4dee3
Stefanie T <72820144+steffleupagus@users.noreply.github.com>
2022-05-01 02:33:53 +0000

    Cleaning up some commands, adding upgrade command

---
+++ handlers/commands/cmdChanUpgrade.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { Permissions } = require('discord.js')
+const mod = process.env.mod || "";
+const config = require(`${process.cwd()}/config/${mod}_config.json`);
+
+async function execute(interaction)
+{
+	const client = interaction.client
+	client.commands.get('chanmeta').execute(interaction, true)	
+}
+
+async function run(client, message, command, args)
+{
+	client.commands.get('chanmeta').run(client, message, command, args)
+}
+
+const data = new SlashCommandBuilder()
+	.setName('upgrade')
+	.setDescription('Upgrade the given channel to make it eligible for RP exp via the /scene cmd')
+	.setDefaultPermission(false)
+	.addChannelOption(option => option.setName('target').setRequired(false)
+									  .setDescription('Specify a target channel'))
+	.addBooleanOption(option => option.setName('xp').setRequired(false)
+									  .setDescription('Specify if channel is exp eligible'))
+	.addBooleanOption(option => option.setName('notify').setRequired(false)
+									  .setDescription('Should send channel update notification'))
+
+const userPermissions = [	Permissions.FLAGS.MANAGE_CHANNELS,
+							Permissions.FLAGS.SEND_MESSAGES		];
+module.exports = 
+{
+	data: data,
+	whitelistRoles: [
+		config.BuilderRole,
+		config._BuilderRole
+	],
+	userPermissions: userPermissions,
+	botPermissions: userPermissions,
+	execute: execute,
+	message: run,
+
+	build:config.DEV||config.PRODUCTION 
+};
