fb19f1c8e0dab5d369b60f4cf1d4ea19c21f188c
Stefanie T <72820144+steffleupagus@users.noreply.github.com>
2022-08-29 01:03:48 +0000

    Renaming GuildAdmin to GuildSetup

--- handlers/commands/cmdGuildSetup.js
+++ handlers/commands/cmdGuildSetup.js
@@ -4,15 +4,39 @@ const { SlashCommandBuilder,
 const { EmbedBuilder, 
 	   	PermissionsBitField } = require('discord.js')
 const guildData = require(`../../database/guildDataSchema.js`);
+const guildRank = require(`../../database/guildRankSchema.js`);
 const mod = process.env.mod || "";
 const config = require(`../../config/${mod}_config.json`);
 
-async function updateGuildRank(guild, rank, role=null, image=null, emoji=null)
+async function updateGuildRank(server, guild, rank=null, role=null, image=null, emoji=null)
 {
-	const query = { guild: guild, rankId: rank };
-	const update = { $set: { roleId: role, imageUrl: image, emoji: emoji } };
-	const options = { new: true, upsert: true }
-	record = await guildData.findOneAndUpdate(query, update, options);	
+	let query = { guild: guild, rank: rank }
+	let update;
+	let options = { new: true, upsert: true }
+	let record;
+	if (rank && guild)
+	{
+		update = { $set: { } };
+		if (role) update["$set"].role = role.id
+		if (image) update["$set"].imageUrl = image		
+		record = await guildRank.findOneAndUpdate(query, update, options);
+	}
+	else if (rank)
+	{
+		update = { $set: { role: role.id } };
+		record = await guildRank.findOneAndUpdate(query, update, options);
+	}
+	else
+	{
+		query = { guild: guild }
+		// update = { $set: { role: role.id, imageUrl: image, emoji: emoji } };
+		update = { $set: { } }
+		if (role) update["$set"].role = role.id
+		if (image) update["$set"].imageUrl = image
+		if (emoji) update["$set"].emoji = emoji;
+		record = await guildData.findOneAndUpdate(query, update, options);
+	}
+
 	return record;
 }
 
@@ -27,16 +51,31 @@ async function execute(interaction)
 	const rank = interaction.options.getNumber('rank');
 	const role = interaction.options.getRole('role');
 	const image = interaction.options.getString('image');
-	const emoji = interaction.options.getString('emoji');
+	let emoji = interaction.options.getString('emoji');
 
 	await interaction.deferReply({ephemeral:true})
-	
-	const record = await updateGuildRank(guild, rank, role, image, emoji);
+
+	const server = interaction.guild;
+	const record = await updateGuildRank(server, guild, rank, role, image, emoji);
 	console.log(record);
 
 	let embed = new EmbedBuilder();
 		embed.setTitle("Guild Rank Updated")
+	if (rank && guild)
+	{
+		emoji = await guildData.findOne({guild: guild})
+		console.log(emoji)
+		emoji = emoji ? emoji.emoji : '';
 		embed.setDescription(`${emoji} Rank \`${rank}\` of the \`${guild}\`: ${role}`)
+	}
+	else if (rank)
+	{
+		embed.setDescription(`Rank role set: ${role}`)
+	}
+	else
+	{
+		embed.setDescription(`Guild data set: ${emoji} ${role}`)	
+	}
 	if (image)
 		embed.setThumbnail(image)
 	interaction.editReply({embeds:[embed]})
@@ -57,7 +96,7 @@ async function execute(interaction)
 const guildOption = new SlashCommandStringOption()
 		.setName('guild')
 		.setDescription('The name of the guild being applied')
-		.setRequired(true)
+		.setRequired(false)
 		.addChoices(
 			{ name: 'Arcanum', value: 'Arcanum' },
 			{ name: 'Black Hand', value: 'Black Hand' },
@@ -69,9 +108,8 @@ const guildOption = new SlashCommandStringOption()
 const rankOption = new SlashCommandNumberOption()
 		.setName('rank')
 		.setDescription('The guild rank to apply')
-		.setRequired(true)
+		.setRequired(false)
 		.addChoices(
-			{ name: 'Base Guild', value: 0 },		
 			{ name: '1: Recruit', value: 1 },
 			{ name: '2: Initiate', value: 2 },
 			{ name: '3: Member', value: 3 },
@@ -80,18 +118,18 @@ const rankOption = new SlashCommandNumberOption()
 		)
 
 const data = new SlashCommandBuilder()
-	.setName('guildadmin')
+	.setName('guildsetup')
 	.setDescription('Configures the guild ranks database')
 	.addStringOption(guildOption)
 	.addNumberOption(rankOption)
 	.addRoleOption(option => option
 		.setName('role')
-		.setDescription('The role to apply to the specified rank')
+		.setDescription('The role to apply to the guild/rank')
 		.setRequired(false)
 	)
 	.addStringOption(option => option
 		.setName('image')
-		.setDescription('The image URL to associate with the specified rank')
+		.setDescription('The image URL to associate with the guild/rank')
 		.setRequired(false)
   	)
 	.addStringOption(option => option
