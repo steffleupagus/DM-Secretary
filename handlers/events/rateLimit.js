c98459c0448a8f9e010692744321812b08286dd4
Stefanie T <72820144+steffleupagus@users.noreply.github.com>
2022-09-23 19:30:45 +0000

    Auto restart on rate limit

--- handlers/events/rateLimit.js
+++ handlers/events/rateLimit.js
@@ -1,7 +1,12 @@
 module.exports = {
 	name: 'rateLimit',
 	once: false,
-	execute(client, info) {
+	execute(client, info) 
+	{
 		console.error(`rateLimit -> `, info);
+		if (info.timeout > 10000) 
+		{			
+			process.kill(1)		
+		}
 	},
 };
