e6d289893d90471de69f83c7057cb975f868979d
Stefanie T <72820144+steffleupagus@users.noreply.github.com>
2023-04-05 03:53:01 +0000

    Quest command development

--- database/questSchema.js
+++ database/questSchema.js
@@ -3,7 +3,7 @@ const config = require(`${process.cwd()}/config/${mod}_config.json`)
 
 const mongoose = require('mongoose')
 
-const schemaName = `quest${config.DEV ? "dev" : ""}`
+const schemaName = `quest`//${config.DEV ? "dev" : ""}`
 
 const reqString = { type:String, required:true }
 const schema = new mongoose.Schema({
