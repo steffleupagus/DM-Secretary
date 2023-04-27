34b6398ac77fdef73420cb4357486679112ab1ec
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2023-04-27 06:14:27 +0000

    Activity update

---
+++ handlers/commands/admincmd_Update.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js')
+const mod = process.env.mod || "";
+const config = require(`../../config/${mod}_config.json`);
+
+async function execute(interaction)
+{
+	await interaction.reply({content:"Updating", ephemeral:true})
+	if (interaction.channel.id == config.activityChannel)
+	{
+		const timer = interaction.client.timers.get('activityUpdate')
+		timer.stopTimer()
+		await timer.triggerTimer(interaction.client)
+		timer.startTimer(interaction.client)
+	}
+}
+
+const data = new SlashCommandBuilder()
+			.setName('update')
+			.setDescription('Update the activity tracker')
+const userPermissions = [	PermissionsBitField.Flags.SendMessages		];
+module.exports = 
+{
+	data: data,
+	whitelistRoles: [ config.BuilderRole, config._BuilderRole ],
+	userPermissions: userPermissions,
+	execute: execute,
+	build:config.PRODUCTION //|| config.DEV
+};
