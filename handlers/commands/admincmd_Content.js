e6d3d1f88fb5258e296e598005c4d7e1b92f7251
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2023-05-09 01:06:15 +0000

    Updating admin content command to expand functionality

--- handlers/commands/admincmd_Content.js
+++ handlers/commands/admincmd_Content.js
@@ -8,6 +8,7 @@ const wait = require('util').promisify(setTimeout);
 const mod = process.env.mod || "";
 const config = require(`../../config/${mod}_config.json`);
 const index = require(`../../content/_contentIndex.json`)
+const TEST_CHAN = "940061953064329216"
 
 function extractMention(embed) 
 {
@@ -33,13 +34,15 @@ function extractMention(embed)
 async function publishContent(channel, content)
 {
 	let index = [];
+	let indexInline = true;
 	await Utils.asyncObjectForEach(content, async (value, key)=>
 	{
 		const header = `\`\`\`md\n# --- ${key} --- #\n\`\`\``;
 		const tableOfContents = value.includeTOC || value.includeHeader ? 
 									await channel.send(header) : null;
 		let contents = [];
-
+		indexInline = indexInline && (value.inlineIndex ?? true);
+		
 		if (typeof value === 'string')
 		{
 			await channel.send(value);
@@ -51,13 +54,28 @@ async function publishContent(channel, content)
 			{
 				if (embed?.description?.includes("<last_id>"))
 					embed.description = embed.description.replace("<last_id>",lastMessage)
+
+				const prefix = embed.prefix ?? "";
+				delete embed.prefix;
+
+				const title = embed.title || embed.Title || "";
+				delete embed.Title;
+				embed.title = `${prefix}${title}`.trim()				
+				
+				let thread = embed.thread || null;
+				delete embed.thread;
+				let fieldBreak = embed.indexBreak ?? false
+				delete embed.indexBreak
 				
 				let item = await channel.send({embeds:[embed]});
-				if (value.includeTOC)
-					contents.push({"title":embed.title,"url":item.url});
-				else if (value.includeIndex)
-					index.push({"title":embed.title,"url":item.url});		
-
+				if (value.includeTOC && title)
+					contents.push({"prefix":prefix,"title":title,"url":item.url});
+				else if (value.includeIndex && title)
+					index.push({"prefix":prefix,"title":title,"url":item.url,"break":fieldBreak});
+				
+				if (thread)
+					await item.startThread({name:thread})
+				
 				lastMessage = item.id
 				
 				let chanMentions = extractMention(embed).trim();
@@ -108,13 +126,19 @@ async function publishContent(channel, content)
 	if (index.length)
 	{
 		let embed = new Embed()
-			embed.addField("**Index**", '', true);
+			embed.addField("**Index**", '', indexInline);
 			embed.setFooter({text:"Index"})
 		index.forEach( (item)=>
 		{
 			if (!item.title) return;
-			const field = `[${item.title}](${item.url})`
-			embed.extendField(field, "** **", true);			
+			if (item.break)
+			{
+				embed.closeField();
+				embed.addField("** **",'', indexInline)
+			}
+			
+			const field = `${item.prefix || ""}[${item.title}](${item.url})`			
+			embed.extendField(field, "** **", indexInline);
 		});
 		await embed.send(channel);
 	}
@@ -138,6 +162,7 @@ async function execute(interaction)
 	const channel = await guild?.channels.fetch(channelId);
 	const target = interaction.options.getChannel('target') || channel
 	const clear  = interaction.options.getBoolean('clear') ?? false
+	const override = interaction.options.getString('content') ?? null
 
 	await interaction.deferReply({ephemeral:true});
 	
@@ -149,7 +174,8 @@ async function execute(interaction)
 	}
 
 	//Grab the data for the new content according to what goes in this channel
-	const content = require(`${process.cwd()}/content/${index[channel.id].data}`)
+	let source = override || index[channel.id].data	
+	const content = require(`${process.cwd()}/content/${source}`)
 	if (!content)
 	{
 		await interaction.editReply(`No content found for <#${target.id}>. Aborting`);
@@ -180,13 +206,40 @@ async function run(client, message, command, args)
 {
 }
 
+////// Handle autocomplete options for the Character field
+async function autoComplete(interaction) 
+{
+	const focusedOption = interaction.options.getFocused(true);
+	if (interaction.channel.id == TEST_CHAN && focusedOption.name === 'content') 
+	{
+		const value = focusedOption.value.toLowerCase();
+		console.log(value);
+		const response = Object.values(index)
+								.map( x => ({ name: x.data.replace(".json",""), value: x.data }) )
+								.filter( x => x.value.includes(value) )
+		console.log(response)
+		
+		try {
+			interaction.respond(response.length <= 25 ? response : []);
+		}
+		catch (e) {}
+	}
+	else
+		interaction.respond([])
+}
+
 const data = new SlashCommandBuilder()
 	.setName('content')
 	.setDescription('Update the contents of a static channel')
 	.setDefaultPermission(false)	
 	.addChannelOption(option => option.setName('target').setRequired(false).setDescription('Specify a target channel'))
 	.addBooleanOption(option => option.setName('clear').setRequired(false).setDescription('Clear old messages or not'))
-
+	.addStringOption(option => option
+			.setName('content')
+			.setDescription('Override from available content')
+			.setRequired(false)
+			.setAutocomplete(true)
+		)
 const userPermissions = [	PermissionsBitField.Flags.ManageChannels,
 							PermissionsBitField.Flags.SendMessages		];
 
@@ -199,6 +252,6 @@ module.exports =
 	userPermissions: userPermissions,
 	execute: execute,
 	message: run,
-
-	build:config.DEV||config.PRODUCTION 
+	autoComplete: autoComplete,
+	build:config.PRODUCTION 
 };
