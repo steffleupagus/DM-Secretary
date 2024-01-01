a31df217dfeeb23b6a6bc789df5634e7e93996fe
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2024-01-01 03:09:27 +0000

    Adding a command to mimic the scene auto-close for testing

---
+++ handlers/commands/menuEndSceneAuto.js
@@ -0,0 +1,27 @@
+const { ContextMenuCommandBuilder } = require('discord.js')
+const { ApplicationCommandType } = require(`../../utilities/enums.js`)
+const mod = process.env.mod || "";
+const config = require(`../../config/${mod}_config.json`)
+
+async function execute(interaction)
+{
+	const client = interaction.client;
+	const messageId = interaction.targetId;
+	const channel = interaction.channel;
+	const message = await channel?.messages.fetch(messageId);
+
+	const command = `scene${config.DEV ? "dev" : ""}`
+	await client.commands.get(command).autoClose(channel, true)
+}
+
+const requiredRoles = [ config.BuilderRole ]
+module.exports = 
+{
+	data: new ContextMenuCommandBuilder()
+		.setName('Conclude Scene (Auto)')
+		.setType(ApplicationCommandType.Message)
+		.setDefaultPermission(false),
+	execute: execute,
+	whitelistRoles: requiredRoles,
+	build:config.DEV
+};
