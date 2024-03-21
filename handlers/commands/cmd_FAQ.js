4a2c5b2cd4a67998be715dbfbf52c4e0f70577d2
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2024-03-21 01:05:18 +0000

    Updating FAQ

--- handlers/commands/cmd_FAQ.js
+++ handlers/commands/cmd_FAQ.js
@@ -7,6 +7,7 @@ const Utils = require(`../../utilities/utilFuncs.js`);
 const config = require(`../../config/${mod}_config.json`);
 
 let faqIndex = null;
+const messageIndex = {}
 
 async function execute(interaction)
 {
@@ -20,7 +21,23 @@ async function execute(interaction)
 	if (topic)
 	{
 		embed = faqData[topic].embed
-		console.log(embed)
+
+		if (!embed.url)
+		{
+			const faqChannel = await interaction.guild.channels.fetch(config.faqChannel);
+			const faqMessages = await faqChannel.messages.fetch();
+			faqMessages.each( x => {
+				const title = x?.embeds[0]?.data?.title
+				if (faqData[title] && faqData[title].embed)
+					faqData[title].embed.url = x.url					
+			})
+			
+			// const faqMessage = faqMessages.find(x => x?.embeds[0]?.data?.title == topic)		
+			// console.log(faqMessage);
+			// faqData[topic].embed.url = embed.url = faqMessage.url
+			embed = faqData[topic].embed
+		}
+		// console.log(embed)
 	}
 		
 	if (null == embed)
@@ -48,9 +65,11 @@ function getFAQData()
 		let section = key;
 		value.embeds.forEach( embed => 
 		{
+			const json = JSON.stringify(embed)
 			faq[embed.title] = {
 				embed,
-				section
+				section,
+				json
 			}
 		});
 	};
@@ -92,9 +111,10 @@ async function autoComplete(interaction)
 		const value = focusedOption.value.toLowerCase();
 		let response = Object.keys(faqData);
 		if (value.length > 0)
-			response = response.filter(x => x.toLowerCase().includes(value))
+			response = response.filter(x => x.toLowerCase().includes(value)
+								|| faqData[x].json.toLowerCase().includes(value))
 		response = response.map(x => ({name:"[" + faqData[x].section + "] " + x, value:x}))
-		console.log(response)
+		// console.log(response)
 		try {		
 			response = response.length <= 25 ? response : response.splice(0,25)
 			interaction.respond(response);
