89a0e9b6d5c26d637226fd4f07598179460f7304
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2025-08-16 01:36:18 +0000

    Formatting

--- bot.js
+++ bot.js
@@ -6,20 +6,16 @@ const { promisify } = require("util");
 const globPromise = promisify(glob);
 const mongoose = require('mongoose')
 
-class Bot
-{
-	constructor()
-	{
-		const intents =
-		[
+class Bot {
+	constructor() {
+		const intents = [
 			GatewayIntentBits.Guilds,
 			GatewayIntentBits.GuildMessages,
 			GatewayIntentBits.GuildMessageReactions,
 			GatewayIntentBits.MessageContent,
 			GatewayIntentBits.GuildMembers,
 		]
-		const partials =
-		[
+		const partials = [
 			Partials.Channel,
 			Partials.Message,
 			Partials.User,
@@ -29,13 +25,11 @@ class Bot
 		this.loadBot();
 	}
 
-	async loadBot()
-	{
+	async loadBot() {
 		this.loadConfig();
 		await this.loadEvents();
 		await this.loadMessageHandlers();
-		this.client.on("ready", async () =>
-		{
+		this.client.on("ready", async () => {
 			await this.loadCommands();
 			await this.loadDatabase();
 			await this.loadTimers();
@@ -45,16 +39,14 @@ class Bot
 		});
 		this.runBot();
 
-		process.on('uncaughtException', function(error)
-		{
+		process.on('uncaughtException', function(error) {
 			console.error("Unhandled Bullshit: ", error)
 			//console.error(error.stack);
 		});
 	}
 
 	/// Load configuration file
-	loadConfig()
-	{
+	loadConfig() {
 		const mod = process.env.mod || "";
 		this.client.config = require(`./config/${mod}_config.json`);
 		this.client.config.token = process.env.token;
@@ -62,8 +54,7 @@ class Bot
 		console.log(`CONFIG LOADED: ${this.client.config.CONFIG}`)
 	}
 
-	async loadDatabase()
-	{
+	async loadDatabase() {
 		mongoose.connection.on('error', console.error)
 		mongoose.connection.on('connected', console.log)
 		mongoose.connection.on('disconnected', console.log)
@@ -75,21 +66,18 @@ class Bot
 	}
 
 	/// Load individual event files and register the event for dynamic execution
-	async loadEvents()
-	{
+	async loadEvents() {
 		console.log("Loading events...");
 		this.client.eventHandlers = new Collection();
 
-		// const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));	
+		// const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 		const eventFiles = await globPromise(`./handlers/events/*.js`);
-		eventFiles.map((file) => 
-		{
+		eventFiles.map((file) =>  {
 			//const event = require(`./events/${file}`);
 			const event = require(file);
-			if (!event.hasOwnProperty("build") || event.build)
-			{
+			if (!event.hasOwnProperty("build") || event.build) {
 				console.log(" - Event: ", event.name);
-				if (event.once) 
+				if (event.once)
 					this.client.once(event.name, (...args) => event.execute(this.client, ...args));
 				else
 					this.client.on(event.name, (...args) => event.execute(this.client, ...args));
@@ -102,55 +90,46 @@ class Bot
 		});
 	}
 
-	async loadMessageHandlers()
-	{
+	async loadMessageHandlers() {
 		console.log("Loading message handlers...");
 		const messageHandlers = fs.readdirSync(`./handlers/message`)
 								  .filter(file => file.endsWith('.js'));
 
 		this.client.messageHandlers = [];
-		for (const file of messageHandlers)
-		{
+		for (const file of messageHandlers) {
 			const handler = require(`./handlers/message/${file}`);
 			console.log(" - Handler: ", handler.name);
-			if (!handler.hasOwnProperty("build") || handler.build)
-			{
+			if (!handler.hasOwnProperty("build") || handler.build) {
 				this.client.messageHandlers.push(handler);
 			}
 		}
 	}
 
-	loadReactHandlers()
-	{
+	loadReactHandlers() {
 		console.log("Loading reaction handlers...");
 		const messageHandlers = fs.readdirSync(`./handlers/reations`)
 								  .filter(file => file.endsWith('.js'));
 
 		this.client.reactHandlers = [];
-		for (const file of messageHandlers)
-		{
+		for (const file of messageHandlers) {
 			const handler = require(`./handlers/reations/${file}`);
 			console.log(" - Handler: ", handler.name);
-			if (!handler.hasOwnProperty("build") || handler.build)
-			{
+			if (!handler.hasOwnProperty("build") || handler.build) {
 				this.client.reactHandlers.push(handler);
 			}
 		}
 	}
 
-	loadTimers()
-	{
+	loadTimers() {
 		console.log("Loading timers...");
 		const timers = fs.readdirSync(`./handlers/timers`)
 						 .filter(file => file.endsWith('.js'));
 
 		this.client.timers = new Collection();
-		for (const file of timers) 
-		{
+		for (const file of timers) {
 			const timer = require(`./handlers/timers/${file}`);
 			console.log(" - Timer: ", timer.name, (timer.build ?? true) ? "(Enabled)" : "(Disabled)" );
-			if (!timer.hasOwnProperty("build") || timer.build)
-			{
+			if (!timer.hasOwnProperty("build") || timer.build) {
 				this.client.timers.set(timer.name, timer)	//.push(timer);
 				timer.startTimer(this.client);
 			}
@@ -158,23 +137,20 @@ class Bot
 	}
 
 	/// Load the individual command files and register them for dynamic execution
-	async loadCommands()
-	{
+	async loadCommands() {
 		console.log("Loading commands...");
 
 		this.client.commands = new Collection();
 		const commandFiles = fs.readdirSync(`./handlers/commands`).filter(file => file.endsWith('.js'));
 		if (!commandFiles.length)
 			console.log(" - No commands found");
-		for (const file of commandFiles)
-		{
+		for (const file of commandFiles) {
 			let command = null;
 			try { command = require(`./handlers/commands/${file}`); }
 			catch(e) { console.log(e.stack) }
 			const enabled = !command?.hasOwnProperty("build") || command?.build;
 			// console.log(` - Command: ${command.data.name}${enabled ? "" : " [Disabled]"}`);
-			if (command && enabled)
-			{
+			if (command && enabled) {
 				console.log(` - Command: ${command.data.name}${enabled ? "" : " [Disabled]"}`);
 				// Set a new item in the Collection; key = command name, value = exported module
 				this.client.commands.set(command.data.name, command);
@@ -188,8 +164,7 @@ class Bot
 	}
 
 	/// Load tests and run them if active
-	async runTests()
-	{
+	async runTests() {
 		console.log("Executing Unit Tests...");
 		const tests = fs.readdirSync(`./handlers/tests`)
 		 				.filter(file => file.endsWith('.js'));
@@ -197,26 +172,22 @@ class Bot
 		{
 			const test = require(`./handlers/tests/${file}`);
 			console.log(" - Test: ", test.name, (test.build ?? true) ? "(Enabled)" : "(Disabled)" );
-			if (!test.hasOwnProperty("build") || test.build)
-			{
-				test.run(this.client);
+			if (!test.hasOwnProperty("build") || test.build) {
+				await test.run(this.client);
 			}
 		}
 	}
 
 	/// Handle login and disconnect
-	runBot()
-	{
+	runBot() {
 		//Login
 		console.log("Logging in...")
 		this.client.login(this.client.config.token).catch(console.error);
 
 		//handle trying to re-login on disco
-		this.client.on("disconnect", () => setTimeout(() =>
-		{
+		this.client.on("disconnect", () => setTimeout(() => {
 			console.log("Bot disconnected :(");
-			this.client.destroy().then(() =>
-			{
+			this.client.destroy().then(() => {
 				this.runBot();
 			});
 		}, 10000));
@@ -224,6 +195,4 @@ class Bot
 }
 
 const bot = new Bot();
-//require("./dashboard")(bot.client)
-
 module.exports = bot.client;
